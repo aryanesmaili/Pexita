5c3ca6901b566c55680f89cbb12d01892bfdb973
Aryan Esmaili <aryanesmailiea@gmail.com>
2023-08-02 07:41:22 +0330

    xxx

--- Pexita/Data/Entities/Products/ProductCreateVM.cs
+++ Pexita/Data/Entities/Products/ProductCreateVM.cs
@@ -18,29 +18,29 @@ namespace Pexita.Data.Entities.Products
     }
     public class ProductInfoVM
     {
-        public required int ID { get; set; }
-        public required string Title { get; set; }
+        public int ID { get; set; }
+        public string Title { get; set; }
         public string? Description { get; set; }
         public double? Price { get; set; }
         public int? Quantity { get; set; }
-        public required BrandInfoVM Brand { get; set; }
+        public BrandInfoVM Brand { get; set; }
         public double? Rate { get; set; }
-        public required List<TagInfoVM> Tags { get; set; }
-        public required string ProductPics { get; set; }
-        public required bool IsAvailable { get; set; }
-        public required DateTime DateCreated { get; set; }
+        public List<TagInfoVM> Tags { get; set; }
+        public string ProductPics { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime DateCreated { get; set; }
     }
     public class ProductUpdateVM
     {
-        public required string Title { get; set; }
+        public string Title { get; set; }
         public string? Description { get; set; }
         public double? Price { get; set; }
         public int? Quantity { get; set; }
-        public required string Brand { get; set; }
+        public string Brand { get; set; }
         public double? Rate { get; set; }
-        public required List<IFormFile> ProductPics { get; set; }
-        public required bool IsAvailable { get; set; } = true;
-        public required string Tags { get; set; }
+        public List<IFormFile> ProductPics { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public string Tags { get; set; }
         public string? Colors { get; set; }
 
     }
